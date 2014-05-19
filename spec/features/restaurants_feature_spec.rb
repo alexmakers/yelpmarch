236e3d98dad26d3b8a3defdc6b90634363a23b53
Alex Peattie <alexpeattie@gmail.com>
2014-05-19 11:26:19 +0100

    Initial commit

---
+++ spec/features/restaurants_feature_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe 'restaurants index page' do
+  context 'no restaurants have been added' do
+    it 'should display a message' do
+      visit '/restaurants'
+      expect(page).to have_content 'No restaurants yet'
+    end
+  end
+end
+
+describe 'creating a restaurant' do
+  it 'adds it to the restaurants index' do
+    visit '/restaurants/new'
+    fill_in 'Name', with: 'McDonalds'
+    fill_in 'Address', with: '1 City Road, London'
+    click_button 'Create Restaurant'
+
+    expect(current_path).to eq '/restaurants'
+    expect(page).to have_content 'McDonalds'
+  end
+end
