d5dc656c3a879e4f85080f4ca1e4780110999b3c
Alex Peattie <alexpeattie@gmail.com>
2014-05-22 10:59:35 +0100

    Enhance AJAX functionality

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,10 +6,8 @@ class ReviewsController < ApplicationController
 
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
-    review = @restaurant.reviews.create!(params[:review].permit(:thoughts, :rating))
-    respond_to do |format|
-      format.html{ redirect_to '/restaurants' }
-      format.json{ render json: review }
-    end
+    @review = @restaurant.reviews.create!(params[:review].permit(:thoughts, :rating))
+
+    render 'create', content_type: :json
   end
 end
